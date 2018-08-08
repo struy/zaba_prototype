e3900c7c9fa499b8cc53f1a3b007e916c3c49f35
struy <dubovykwork@gmail.com>
2018-08-08 18:46:37 -0500

    Fix wsgi for gunicorn

--- zaba/wsgi.py
+++ zaba/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dzenzaba project.
+WSGI config for zaba project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dzenzaba.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zaba.settings")
 
 application = get_wsgi_application()

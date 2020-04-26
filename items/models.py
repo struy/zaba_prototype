b59e9ea4fed5e274e0a4f6a107703dc7cfc4595c
struy <struykm@yandex.ru>
2020-04-26 13:31:51 -0500

    conditions

--- items/models.py
+++ items/models.py
@@ -6,8 +6,17 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Item(Advert, Location):
-    # condition =
-    # size =
+    CONDITIONS = [(0, _('Used')),
+                  (1, _('Acceptable')),
+                  (2, _('Very Good')),
+                  (3, _('Like New')),
+                  (4, _('New')),
+                  ]
+    condition = models.CharField(
+        max_length=1,
+        choices=CONDITIONS,
+        default=0
+    )
     image = models.ImageField(upload_to='items', default='none/no-img.jpg')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('price'))
 

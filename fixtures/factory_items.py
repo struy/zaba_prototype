e78aa7630d9a970020a5d0cb6ba5b320bccac595
struy <struykm@yandex.ru>
2020-04-27 12:41:09 -0500

    save fake images

--- fixtures/factory_items.py
+++ fixtures/factory_items.py
@@ -1,6 +1,8 @@
 import random as rand
+from urllib import request
 from faker import Factory
 from django.contrib.gis.geos import Point
+from django.core.files.base import ContentFile
 from items.models import Item
 
 
@@ -8,6 +10,9 @@ local = ['en_US', 'uk_UA', 'ru_RU', 'pl_PL']
 for loc in local:
     fake = Factory.create(loc)
     for i in range(20):
+        image_url = fake.image_url(width=800, height=600)
+        response = request.urlopen(image_url)
+        image_name = image_url.rsplit('.', 1)[1].lower()+'.jpeg'
         local_lat = fake.local_latlng()
         item = Item(title=fake.catch_phrase(),
                     description=fake.text(),
@@ -18,9 +23,10 @@ for loc in local:
                     expires=fake.date(),
                     city=local_lat[2],
                     address=fake.street_address())
-        item.save()
+        item.image.save(image_name, ContentFile(response.read()))
+
+        # Python3 save from folder
+        # item.image.save('abc.png', File(open('/tmp/pic.png', 'rb')))
 
 
 
-
-# fake = Factory.create(['en_US ', 'uk_UA', 'ru_RU', 'pl_PL'])

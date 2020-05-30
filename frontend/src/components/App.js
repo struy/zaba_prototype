0c2fef92af72fbc361103239fa8b2b541caa7c52
struy <struykm@yandex.ru>
2020-05-30 18:47:47 -0500

    bootstrap+ react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,52 +1,60 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React, {Component} from "react";
+import {render} from "react-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading"
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            loaded: false,
+            placeholder: "Loading"
+        };
+    }
 
-  componentDidMount() {
-    fetch("items/api")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
-  }
+    componentDidMount() {
+        fetch("items/api")
+            .then(response => {
+                if (response.status > 400) {
+                    return this.setState(() => {
+                        return {placeholder: "Something went wrong!"};
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                this.setState(() => {
+                    return {
+                        data,
+                        loaded: true
+                    };
+                });
+            });
+    }
 
-  render() {
-    return (
-      <ul>
-        {this.state.data.map(contact => {
-          return (
-            <li key={contact.id}>
-              {contact.id} - {contact.title}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
+    render() {
+        return (
+            <main className="content">
+                <h1 className=" text-uppercase text-center my-4">Item list</h1>
+                <div className="row ">
+                    <div className="col-md-6 col-sm-10 mx-auto p-0">
+                        <div className="card p-3">
+                            <ul className="list-group list-group-flush">
+                                {this.state.data.map(item => {
+                                    return (
+                                        <li style={{listStyleType:"none"}} key={item.id}>
+                                            {item.id} - {item.title}
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default App;
 
-const container = document.getElementById("app");
-render(<App />, container);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,7 @@
-import App from "./components/App";
+  import React from 'react';
+  import ReactDOM, {render} from 'react-dom';
+  // import 'bootstrap/dist/css/bootstrap.min.css';
+  import App from "./components/App";
+
+  const container = document.getElementById("app");
+  render(<App />, container);

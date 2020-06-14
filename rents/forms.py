d2b22dfa02b389c583cee2dcc877e2d2bc333773
struy <struykm@yandex.ru>
2020-06-14 13:28:23 -0500

    add style

--- rents/forms.py
+++ rents/forms.py
@@ -6,8 +6,8 @@ from .models import Rental
 class RentForm(forms.ModelForm):
     class Meta:
         model = Rental
-        fields = '__all__'
-        # ['title', 'description', 'price', 'image', 'expires', 'city', 'address', 'point']
+        fields = ['rental_type', 'title', 'description', 'expires', 'price', 'image', 'pet_policy',
+                  'bathrooms', 'bedrooms', 'furnished', 'city', 'address', 'point']
         widgets = {
             'expires': forms.DateInput(format='%m/%d/%Y', attrs={'class': 'datepicker'}),
             'point': gis_forms.OSMWidget(attrs={'default_lon': 20, 'default_lat': 30, 'map_width': 800,

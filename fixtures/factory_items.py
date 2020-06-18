ec3104065e2637526eee4180bed38a018980b28d
struy <struykm@yandex.ru>
2020-06-17 23:02:52 -0500

    divorsety img

--- fixtures/factory_items.py
+++ fixtures/factory_items.py
@@ -5,14 +5,14 @@ from django.contrib.gis.geos import Point
 from django.core.files.base import ContentFile
 from items.models import Item
 
-
 local = ['en_US', 'uk_UA', 'ru_RU', 'pl_PL']
 for loc in local:
     fake = Factory.create(loc)
+    # items
     for i in range(20):
-        image_url = fake.image_url(width=800, height=600)
+        image_url = fake.image_url()
         response = request.urlopen(image_url)
-        image_name = image_url.rsplit('.', 1)[1].lower()+'.jpeg'
+        image_name = image_url.rsplit('.', 1)[1].lower() + '.jpeg'
         local_lat = fake.local_latlng()
         item = Item(title=fake.catch_phrase(),
                     description=fake.text(),
@@ -20,14 +20,12 @@ for loc in local:
                     point=Point((float(local_lat[1]), float(local_lat[0]))),
                     condition=rand.randint(0, 4),
                     price=rand.randint(1, 2000),
-                    expires=fake.date(),
+                    expires=fake.date_this_year(),
                     city=local_lat[2],
                     address=fake.street_address(),
                     local=loc[:2])
         item.image.save(image_name, ContentFile(response.read()))
-
-        # Python3 save from folder
-        # item.image.save('abc.png', File(open('/tmp/pic.png', 'rb')))
-
-
+    # jobs  fake.job()
+    # rents
+    # gifts
 

8286fd6c54f075cfce89f4dae82ecd3082afb58a
struy <struykm@yandex.ru>
2020-06-19 23:41:46 -0500

    work at datepicker

--- items/forms.py
+++ items/forms.py
@@ -8,7 +8,8 @@ class ItemForm(forms.ModelForm):
         model = Item
         fields = ['condition', 'title', 'description', 'price', 'image', 'expires', 'city', 'address', 'point']
         widgets = {
-            'expires': forms.DateInput(format='%m/%d/%Y', attrs={'class': 'datepicker'}),
+            'expires': forms.DateInput(attrs={'class': 'datepicker'}),
             'point': gis_forms.OSMWidget(attrs={'default_lon': -88, 'default_lat': 41.9, 'map_width': 800,
                                                 'map_height': 500, 'default_zoom': 10}),
         }
+# format='%m/%d/%Y',

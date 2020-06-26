29a3a883df77b676fe7cbe6d5da4c38074fd0f97
struy <struykm@yandex.ru>
2020-06-25 20:17:38 -0500

    jenkins work with prod settings

--- zaba/settings_prod.py
+++ zaba/settings_prod.py
@@ -2,8 +2,12 @@ from .settings import *
 
 # Database
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT')
+    },
 }

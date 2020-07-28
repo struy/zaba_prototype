d70c687f0b57f816a080b61e47f0aae6f04a6e1b
struy <struykm@yandex.ru>
2020-07-27 23:28:55 -0500

    start work at Subscription

---
+++ shop/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('name',)}

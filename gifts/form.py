7041ca686293356b881bfa1e0e2c61fce5fcb3ef
struy <struykm@yandex.ru>
2020-08-19 21:11:38 -0500

    fix showing image

--- gifts/form.py
+++ gifts/form.py
@@ -6,7 +6,7 @@ from gifts.models import Gift
 class GiftForm(forms.ModelForm):
     class Meta:
         model = Gift
-        fields = ['gift_type', 'title', 'description', 'expires', 'city', 'address', 'point']
+        fields = ['gift_type', 'title', 'description', 'image', 'expires', 'city', 'address', 'point' ]
         widgets = dict(expires=forms.DateInput(format='%m/%d/%Y', attrs={'class': 'datepicker'}),
                        point=gis_forms.OSMWidget(attrs={'default_lon': -88, 'default_lat': 41.9, 'map_width': 800,
                                                         'map_height': 500, 'default_zoom': 10}))

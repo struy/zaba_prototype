4c8e16cffdc6d2ef29e6af8a02e537b71d6c8137
struy <struykm@yandex.ru>
2020-08-30 12:03:03 -0500

    refactor filters

--- jobs/filters.py
+++ jobs/filters.py
@@ -1,12 +1,8 @@
 import django_filters
-from django.utils.translation import gettext_lazy as _
 from .models import Job
 
 
 class JobsFilter(django_filters.FilterSet):
-    salary__gt = django_filters.NumberFilter(label=_("Salary is greater than"), lookup_expr='salary__gt')
-    salary__lt = django_filters.NumberFilter(label=_("Salary is less than"), lookup_expr='salary__lt')
-
     class Meta:
         model = Job
         fields = {

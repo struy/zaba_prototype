280a05295c64a8f9ae5b33db181bd567c700df41
struy <struykm@yandex.ru>
2020-12-07 20:50:43 -0600

    add migration

--- persons/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django_countries.fields import CountryField
-
-
-class Person(models.Model):
-    """https://github.com/SmileyChris/django-countries"""
-    # name = models.CharField(max_length=100)
-    # countries = CountryField(multiple=True)
-    pass

97035d935053a34bc53170dcafb2db13d2591619
struy <struykm@yandex.ru>
2020-12-17 12:05:17 -0600

    add to context i18n

--- zaba/settings.py
+++ zaba/settings.py
@@ -116,6 +116,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
                 'django.contrib.messages.context_processors.messages',
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',

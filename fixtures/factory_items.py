c32b2a85a25bacded275501947f17b544174f337
struy <struykm@yandex.ru>
2020-12-20 14:15:30 -0600

    fixtures update

--- fixtures/factory_items.py
+++ fixtures/factory_items.py
@@ -8,14 +8,13 @@ from gifts.models import Gift
 from jobs.models import Job
 from rents.models import Rental
 
-
 AUTHOR_ID = 2
 local = ['en_US', 'uk_UA', 'ru_RU', 'pl_PL']
 for loc in local:
     fake = Factory.create(loc)
     # items
     for i in range(5):
-        image_url = fake.image_url(800,600)
+        image_url = fake.image_url(800, 600)
         response = request.urlopen(image_url)
         image_name = image_url.rsplit('.', 1)[1].lower() + '.jpeg'
         local_lat = fake.local_latlng()
@@ -23,15 +22,60 @@ for loc in local:
                     description=fake.text(),
                     author_id=AUTHOR_ID,
                     point=Point((float(local_lat[0]), float(local_lat[1]))),
-                    condition=rand.randint(0, 4),
-                    price=rand.randint(1, 2000),
                     expires=fake.date_this_year(),
                     city=local_lat[2],
                     address=fake.street_address(),
-                    local=loc[:2])
+                    local=loc[:2],
+                    # uniq
+                    condition=rand.randint(0, 4),
+                    price=rand.randint(1, 2000),
+                    )
         item.image.save(image_name, ContentFile(response.read()))
-    # jobs  fake.job()
+    # jobs
+    for i in range(5):
+        image_url = fake.image_url(800, 600)
+        response = request.urlopen(image_url)
+        image_name = image_url.rsplit('.', 1)[1].lower() + '.jpeg'
+        local_lat = fake.local_latlng()
+        job = Job(title=fake.job(),
+                  description=fake.text(),
+                  author_id=AUTHOR_ID,
+                  point=Point((float(local_lat[0]), float(local_lat[1]))),
+                  local=loc[:2],
+                  address=fake.street_address(),
+                  city=local_lat[2],
+                  expires=fake.date_this_year(),
+                  # uniq
+                  jobtype=rand.randint(0, 4),
+                  duration=rand.randint(0, 3),
+                  salary=rand.randint(1, 2000),
+                  countries=rand.choice(['US', 'UA', 'RU', 'PL'])
+                  )
+        job.image.save(image_name, ContentFile(response.read()))
     # rents
+    for i in range(5):
+        image_url = fake.image_url(800, 600)
+        response = request.urlopen(image_url)
+        image_name = image_url.rsplit('.', 1)[1].lower() + '.jpeg'
+        local_lat = fake.local_latlng()
+        rent = Rental(title=fake.catch_phrase(),
+                      description=fake.text(),
+                      author_id=AUTHOR_ID,
+                      point=Point((float(local_lat[0]), float(local_lat[1]))),
+                      local=loc[:2],
+                      address=fake.street_address(),
+                      city=local_lat[2],
+                      expires=fake.date_this_year(),
+                      # uniq
+                      property_type=rand.randint(0, 4),
+                      bathrooms=rand.randint(1, 3),
+                      bedrooms=rand.randint(1, 3),
+                      pet_policy=rand.randint(0, 5),
+                      furnished=bool(rand.getrandbits(1)),
+                      prefer_sex=rand.choice(['a', 'w', 'm']),
+                      price=rand.randint(1, 2000),
+                      )
+        rent.image.save(image_name, ContentFile(response.read()))
     # gifts
     for i in range(5):
         image_url = fake.image_url(800, 600)
@@ -48,4 +92,3 @@ for loc in local:
                     address=fake.street_address(),
                     local=loc[:2])
         gift.image.save(image_name, ContentFile(response.read()))
-

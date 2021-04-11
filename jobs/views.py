4cd6c4103f4df4862a64c6963433d382470b045b
struy <struykm@yandex.ru>
2021-04-10 20:34:23 -0500

    improve jobs sqls

--- jobs/views.py
+++ jobs/views.py
@@ -22,9 +22,9 @@ def index(request):
     if query:
         advert_list = Job.objects.filter(Q(local__exact=lang)
                                          & (Q(title__icontains=query) | Q(description__icontains=query))
-                                         ).order_by('-modified')
+                                         ).prefetch_related('author').order_by('-modified')
     else:
-        advert_list = Job.objects.filter(local=lang).order_by('-modified')
+        advert_list = Job.objects.filter(local=lang).prefetch_related('author').order_by('-modified')
 
     filters = JobsFilter(request.GET, queryset=advert_list)
     adverts, has_filter = context_helper(request, filters)

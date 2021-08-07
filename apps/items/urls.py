e3e80673d4ea2b8eb3653a54976511e590bfc560
struy <struykm@yandex.ru>
2021-08-07 18:31:07 -0500

    clean up items url

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:advert_id>/', views.detail, name='detail'),
     path('map/', views.ItemList.as_view(), name='map'),
-    # path('results/', views.search, name="search"),
     path('new', login_required(views.ItemCreate.as_view()), name='new'),
     path('edit/<int:pk>', login_required(views.ItemUpdate.as_view()), name='edit'),
     path('delete/<int:pk>', login_required(views.ItemDelete.as_view()), name='delete'),
